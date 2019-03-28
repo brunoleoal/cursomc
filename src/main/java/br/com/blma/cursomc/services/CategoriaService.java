3c61b985c4deb63da315f4106a34404f9a664032
Bruno <brunolmalbuquerque@gmail.com>
2019-03-28 14:17:40 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/br/com/blma/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package br.com.blma.cursomc.services;
+
+import br.com.blma.cursomc.domain.Categoria;
+import br.com.blma.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
